b259a9f884d8cbc9014bfa105dbfe1522e8f4bfa
Ishan Kumar <kumarishaan01@gmail.com>
2022-02-04 17:45:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/findmytutor/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.findmytutor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.findmytutor.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView : BottomNavigationView
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_main_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavigationView = binding.bottomNavigation
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    fun setVisibleBottomNavigationView() {
+        binding.bottomNavigation.visibility = View.VISIBLE
+    }
+
+    fun hideBottomNavigationView() {
+        binding.bottomNavigation.visibility = View.GONE
+    }
+
+}
